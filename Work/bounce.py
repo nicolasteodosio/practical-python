15e6e214413994ab6959d0fe19eb03dd8df8d494
Nicolas Teodosio <nicolas.teodosio@luizalabs.com>
2020-06-01 14:12:00 -0300

    feature: add exercise 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+if __name__ == '__main__':
+    height = 100
+    bounce_count = 0
+
+    while bounce_count < 10:
+        height = height * 3/5
+        bounce_count += 1
+        print(f'{bounce_count} {round(height, 4)}')
