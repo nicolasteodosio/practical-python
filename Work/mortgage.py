84f78b756dd0e1b8dadc8bb1fad0c9754495bd8b
Nicolas Teodosio <nicolas.teodosio@luizalabs.com>
2020-06-01 14:12:26 -0300

    wip: mortage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,25 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+if __name__ == '__main__':
+
+    principal = 500000.0
+    rate = 0.05
+    payment = 2684.11
+    total_paid = 0.0
+    extra_payment_start_month = 60
+    extra_payment_end_month = 108
+    extra_payment = 1000
+    months = 1
+    while principal > 0:
+        if months in [extra_payment_start_month, extra_payment_end_month]:
+            payment = payment + extra_payment
+        else:
+            payment = 2684.11
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+        months += 1
+
+    print('Total paid', total_paid)
+    print('Months', months)
